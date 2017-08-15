12daab92ecda8c6a5d7065748bcb8f4f7635cf1d
James Gayfer <gayfer.james@gmail.com>
2017-08-15 14:01:09 -0700

    add setup.py and requirements.txt

---
+++ destiny/__init__.py
@@ -0,0 +1,2 @@
+title = 'destiny'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,18 @@
+import re
+from distutils.core import setup
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('destiny/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+setup(name='destiny-py',
+      author='jgayfer',
+      version=version,
+      license='MIT',
+      description='an asynchronous Destiny 2 API wrapper',
+      install_requires=requirements,
+      packages=['destiny'])
