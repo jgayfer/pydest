a030e611592dc89ce0f4d213abe4a5a1de98bb79
James Gayfer <gayfer.james@gmail.com>
2017-08-22 13:34:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 version = ''
-with open('destiny/__init__.py') as f:
+with open('pydest/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
-setup(name='destiny-py',
+setup(name='pydest',
       author='jgayfer',
       version=version,
       license='MIT',
       description='an asynchronous Destiny 2 API wrapper',
       install_requires=requirements,
-      packages=['destiny'])
+      packages=['pydest'])
