0e1683caa57f2f28c3034bba6e88f789602399f5
James Gayfer <gayfer.james@gmail.com>
2017-08-23 21:45:51 -0700

    add several endpoints to api

--- pydest/api.py
+++ pydest/api.py
@@ -4,7 +4,7 @@ import re
 from pydest.utils import check_alphanumeric
 
 
-BASE_URL = 'https://www.bungie.net/Platform/Destiny/'
+BASE_URL = 'https://www.bungie.net/Platform/Destiny2/'
 
 class API:
 
@@ -15,19 +15,45 @@ class API:
     async def _get_request(self, url):
         """Make an async GET request"""
         headers = {'X-API-KEY':'{}'.format(self.api_key)}
-        async with aiohttp.get(url, headers=headers) as r:
-            return await r.json()
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(url, headers=headers) as r:
+                return await r.json()
 
 
     async def search_destiny_player(self, membership_type, display_name):
+        """Returns a list of Destiny memberships given a full Gamertag or PSN ID"""
         check_alphanumeric(membership_type, display_name)
         url = BASE_URL + 'SearchDestinyPlayer/{}/{}/'
         url = url.format(membership_type, display_name)
         return await self._get_request(url)
 
 
-    async def get_account_summary(self, membership_type, membership_id):
+    async def get_profile(self, membership_type, membership_id):
+        """Returns Destiny Profile information for the supplied membership"""
         check_alphanumeric(membership_type, membership_id)
-        url = BASE_URL + '{}/Account/{}/Summary/'
+        url = BASE_URL + '/{}/Profile/{}/'
         url = url.format(membership_type, membership_id)
+        return await self._get_request(url)
+
+
+    async def get_character(self, membership_type, membership_id, character_id):
+        """Returns character information for the supplied character"""
+        check_alphanumeric(membership_type, membership_id, character_id)
+        url = BASE_URL + '{}/Profile/{}/Character/{}/'
+        url = url.format(membership_type, membership_id, character_id)
+        return await self._get_request(url)
+
+
+    async def get_clan_weekly_reward_state(self, group_id):
+        """Returns information on the weekly clan rewards and if
+           the clan has earned them or not."""
+        check_alphanumeric(group_id)
+        url = BASE_URL + 'Clan/{}/WeeklyRewardState/'
+        url = url.format(group_id)
+        return await self._get_request(url)
+
+
+    async def get_historical_stats_definition(self):
+        """Gets historical stats definitions"""
+        url = BASE_URL + 'Stats/Definition/'
         return await self._get_request(url)
