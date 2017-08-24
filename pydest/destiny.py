713e80fcadb939901f884711ae0783026aa5df3f
James Gayfer <gayfer.james@gmail.com>
2017-08-23 21:53:14 -0700

    update get_account

--- pydest/destiny.py
+++ pydest/destiny.py
@@ -9,6 +9,16 @@ class Destiny:
 
 
     async def get_account(self, membership_type, display_name):
+        """Return a Destiny 2 Account object. None if not found"""
         json = await self.api.search_destiny_player(membership_type, display_name)
-        if json['ErrorCode'] == 1 and json['Response']:
+
+        if json['ErrorCode'] != 1:
+            return None
+
+        membership_id = json['Response'][0]['MembershipId']
+        json = await self.api.get_profile(membership_type, membership_id)
+
+        if json['ErrorCode'] != 1:
+            return None
+        else:
             return Account(json)
