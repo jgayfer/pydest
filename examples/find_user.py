4eb919b91bcf6f8a51e82c4a02353f1b2ebce183
James Gayfer <gayfer.james@gmail.com>
2017-09-03 09:35:55 -0700

    add simple example

---
+++ examples/find_user.py
@@ -0,0 +1,33 @@
+import pydest
+import asyncio
+
+
+platforms = {'XBOX': 1, 'PLAYSTATION': 2, 'PC': 4}
+
+async def main():
+    destiny = pydest.Pydest('2ead071ad8c643559032dc155ee21170')
+
+    platform = None
+    while not platform:
+        user_input = input('Enter your platform (xbox, playstation, or pc): ')
+        if user_input.upper() in platforms.keys():
+            platform = platforms.get(user_input.upper())
+        else:
+            print('Invalid platform.')
+
+    username = input('Enter the username to locate: ')
+    res = await destiny.api.search_destiny_player(platform, username)
+
+    if res['ErrorCode'] == 1 and len(res['Response']) > 0:
+        print("---")
+        print("Player found!")
+        print("Display Name: {}".format(res['Response'][0]['supplementalDisplayName']))
+        print("Membership ID: {}".format(res['Response'][0]['membershipId']))
+    else:
+        print("Could not locate player.")
+
+    destiny.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
