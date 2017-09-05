780db072ee285d9881297715bcec27a1a4158e7e
James Gayfer <gayfer.james@gmail.com>
2017-09-05 11:54:30 -0700

    add unit tests for pydest.py

---
+++ pydest/test/test_pydest.py
@@ -0,0 +1,45 @@
+import pytest
+import asyncio
+from unittest.mock import MagicMock
+
+import pydest
+
+
+class TestDecodeHash(object):
+
+    @pytest.mark.asyncio
+    async def test_decode_hash_called(self):
+        mock = MagicMock()
+        mock.method.return_value = None
+        decode_hash_mock = asyncio.coroutine(mock.method)
+
+        destiny = pydest.Pydest('123')
+        destiny._manifest.decode_hash = decode_hash_mock
+        res = await destiny.decode_hash(123, 'ActivityDefinition', language='fr')
+        destiny.close()
+
+        mock.method.assert_called_with(123, 'ActivityDefinition', 'fr')
+
+
+class TestUpdateManifest(object):
+
+    @pytest.mark.asyncio
+    async def test_update_manifest_called(self):
+        mock = MagicMock()
+        mock.method.return_value = None
+        update_manifest_mock = asyncio.coroutine(mock.method)
+
+        destiny = pydest.Pydest('123')
+        destiny._manifest.update_manifest = update_manifest_mock
+        res = await destiny.update_manifest(language='en')
+        destiny.close()
+
+        mock.method.assert_called_with('en')
+
+
+class TestClose(object):
+
+    def test_close(self):
+        destiny = pydest.Pydest('123')
+        destiny.close()
+        assert destiny._session.closed
