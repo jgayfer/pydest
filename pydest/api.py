cb698939c2b6d389f867184955506759006fa0b2
aescolastico <aesubs@icloud.com>
2022-01-29 01:11:30 -0500

    update get_destiny_entity_definition description

--- pydest/api.py
+++ pydest/api.py
@@ -81,7 +81,22 @@ class API:
 
     
     async def get_destiny_entity_definition(self, entity_type, hash_identifier):
-        """Returns the current version of the manifest
+        """Returns the static definition of an entity of the given Type and hash identifier. 
+        Examine the API Documentation for the Type Names of entities that have their own definitions. 
+        Note that the return type will always *inherit from* DestinyDefinition, but the specific type 
+        returned will be the requested entity type if it can be found. Please don't use this as a chatty 
+        alternative to the Manifest database if you require large sets of data, but for simple and one-off 
+        accesses this should be handy.
+
+        Args:
+            entity_type:
+                The type of entity for whom you would like results. These correspond to the entity's 
+                definition contract name. For instance, if you are looking for items, this property 
+                should be 'DestinyInventoryItemDefinition'. PREVIEW: This endpoint is still in beta, 
+                and may experience rough edges. The schema is tentatively in final form, but there may 
+                be bugs that prevent desirable operation.
+            hash_identifier:
+                The hash identifier for the specific Entity you want returned.
 
         Returns:
             json (dict)
