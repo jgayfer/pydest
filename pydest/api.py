fc8c5890540acb2aaeaaf4686c35dab17c9233c1
aescolastico <aesubs@icloud.com>
2022-02-28 17:15:35 -0500

    Add support for Destiny Entity  (#21)
    * add support for entity definition manifest endpoint
    
    * update get_destiny_entity_definition description
    
    * added destiny entity def to readme

--- pydest/api.py
+++ pydest/api.py
@@ -79,7 +79,33 @@ class API:
         url = DESTINY2_URL + 'Manifest'
         return await self._get_request(url)
 
+    
+    async def get_destiny_entity_definition(self, entity_type, hash_identifier):
+        """Returns the static definition of an entity of the given Type and hash identifier. 
+        Examine the API Documentation for the Type Names of entities that have their own definitions. 
+        Note that the return type will always *inherit from* DestinyDefinition, but the specific type 
+        returned will be the requested entity type if it can be found. Please don't use this as a chatty 
+        alternative to the Manifest database if you require large sets of data, but for simple and one-off 
+        accesses this should be handy.
 
+        Args:
+            entity_type:
+                The type of entity for whom you would like results. These correspond to the entity's 
+                definition contract name. For instance, if you are looking for items, this property 
+                should be 'DestinyInventoryItemDefinition'. PREVIEW: This endpoint is still in beta, 
+                and may experience rough edges. The schema is tentatively in final form, but there may 
+                be bugs that prevent desirable operation.
+            hash_identifier:
+                The hash identifier for the specific Entity you want returned.
+
+        Returns:
+            json (dict)
+        """
+        url = DESTINY2_URL + 'Manifest/{}/{}/'
+        url = url.format(entity_type, hash_identifier)
+        return await self._get_request(url)
+
+    
     async def search_destiny_entities(self, entity_type, search_term, page=0):
         """Gets a page list of Destiny items
 
